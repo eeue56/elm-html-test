62a5df7e12df49b36447a6c4bc99c81b441013c0
Richard Feldman <richard.t.feldman@gmail.com>
2016-11-20 23:00:34 -0800

    Refactor example to use Query.fromHtml only once

--- examples/FailingTests.elm
+++ examples/FailingTests.elm
@@ -11,7 +11,7 @@ import ExampleApp exposing (view, exampleModel)
 
 main : TestProgram
 main =
-    [ testText
+    [ testView
     ]
         |> Test.concat
         |> run emit
@@ -20,40 +20,40 @@ main =
 port emit : ( String, Value ) -> Cmd msg
 
 
-testText : Test
-testText =
-    describe "working with HTML text"
-        [ test "expect 4x <li> somewhere on the page" <|
-            \() ->
-                view exampleModel
-                    |> Query.fromHtml
-                    |> Query.findAll [ tag "li" ]
-                    |> Query.count (Expect.equal 4)
-        , test "expect 4x <li> inside a <ul>" <|
-            \() ->
-                view exampleModel
-                    |> Query.fromHtml
-                    |> Query.find [ tag "ul" ]
-                    |> Query.findAll [ tag "li" ]
-                    |> Query.count (Expect.equal 4)
-        , test "(this should fail) expect header to have 4 links in it, even though it has 3" <|
-            \() ->
-                view exampleModel
-                    |> Query.fromHtml
-                    |> Query.find [ id "heading" ]
-                    |> Query.findAll [ tag "a" ]
-                    |> Query.count (Expect.equal 4)
-        , test "expect footer to have footer text" <|
-            \() ->
-                view exampleModel
-                    |> Query.fromHtml
-                    |> Query.find [ tag "footer" ]
-                    |> Query.has [ tag "footer", text "this is the footer" ]
-        , test "expect each <li> to have classes list-item and themed" <|
-            \() ->
-                view exampleModel
-                    |> Query.fromHtml
-                    |> Query.find [ tag "ul" ]
-                    |> Query.findAll [ tag "li" ]
-                    |> Query.each (Query.has [ classes [ "list-item", "themed" ] ])
-        ]
+testView : Test
+testView =
+    let
+        output =
+            view exampleModel
+                |> Query.fromHtml
+    in
+        describe "view exampleModel"
+            [ test "expect 4x <li> somewhere on the page" <|
+                \() ->
+                    output
+                        |> Query.findAll [ tag "li" ]
+                        |> Query.count (Expect.equal 4)
+            , test "expect 4x <li> inside a <ul>" <|
+                \() ->
+                    output
+                        |> Query.find [ tag "ul" ]
+                        |> Query.findAll [ tag "li" ]
+                        |> Query.count (Expect.equal 4)
+            , test "(this should fail) expect header to have 4 links in it, even though it has 3" <|
+                \() ->
+                    output
+                        |> Query.find [ id "heading" ]
+                        |> Query.findAll [ tag "a" ]
+                        |> Query.count (Expect.equal 4)
+            , test "expect footer to have footer text" <|
+                \() ->
+                    output
+                        |> Query.find [ tag "footer" ]
+                        |> Query.has [ tag "footer", text "this is the footer" ]
+            , test "expect each <li> to have classes list-item and themed" <|
+                \() ->
+                    output
+                        |> Query.find [ tag "ul" ]
+                        |> Query.findAll [ tag "li" ]
+                        |> Query.each (Query.has [ classes [ "list-item", "themed" ] ])
+            ]
