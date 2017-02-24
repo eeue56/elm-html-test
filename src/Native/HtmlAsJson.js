0b38263293b9d4cd05116a5e325faa1dffad28cf
Stoeffel <schtoeffel@gmail.com>
2017-02-23 17:08:18 -0800

    refactoring forceThunks

--- src/Native/HtmlAsJson.js
+++ src/Native/HtmlAsJson.js
@@ -1,52 +1,17 @@
 var _eeue56$elm_html_test$Native_HtmlAsJson = (function() {
-    function forceThunks(vNode) {
-        switch (vNode.type)
-        {
-            case 'thunk':
-                if (!vNode.node) {
-                    vNode.node = vNode.thunk();
-                    forceThunks(vNode.node);
-                }
-                return;
-
-            case 'tagger':
-                forceThunks(vNode.node);
-                return;
-
-            case 'text':
-                return;
-
-            case 'node':
-                var children = vNode.children;
-
-                for (var i = 0; i < children.length; i++) {
-                    forceThunks(children[i]);
-                }
-
-                return;
-
-            case 'keyed-node':
-                var children = vNode.children;
-
-                for (var i = 0; i < children.length; i++) {
-                    forceThunks(children[i]);
-                }
-
-                return domNode;
-
-            case 'custom':
-                return;
-
-            default:
-                throw new Error('Unknown virtual-dom node type: ' + vNode.type);
+    function forceThunks(key, vNode) {
+        if (typeof vNode !== 'undefined' && vNode.type === 'thunk' && !vNode.node) {
+            vNode.node = vNode.thunk.apply(vNode.thunk, vNode.args);
+            return vNode;
+        } else {
+            return vNode;
         }
     }
 
     // stringify needed to strip functions - can performance-optimize this later!
     return {
         toJsonString: function(html) {
-            forceThunks(html);
-            var asString = JSON.stringify(html);
+            var asString = JSON.stringify(html, forceThunks);
 
             if (typeof asString === "undefined"){
                 return "";
