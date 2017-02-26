5f40920a602141df8d76a08e85624ecec1942bad
Stoeffel <schtoeffel@gmail.com>
2017-02-25 17:20:45 -0800

    don't rely on stringify replace function

--- src/Native/HtmlAsJson.js
+++ src/Native/HtmlAsJson.js
@@ -1,17 +1,18 @@
 var _eeue56$elm_html_test$Native_HtmlAsJson = (function() {
-    function forceThunks(key, vNode) {
+    function forceThunks(vNode) {
         if (typeof vNode !== 'undefined' && vNode.type === 'thunk' && !vNode.node) {
             vNode.node = vNode.thunk.apply(vNode.thunk, vNode.args);
-            return vNode;
-        } else {
-            return vNode;
         }
+        if (typeof vNode !== 'undefined' && typeof vNode.children !== 'undefined') {
+            vNode.children = vNode.children.map(forceThunks);
+        }
+        return vNode;
     }
 
     // stringify needed to strip functions - can performance-optimize this later!
     return {
         toJsonString: function(html) {
-            var asString = JSON.stringify(html, forceThunks);
+            var asString = JSON.stringify(forceThunks(html));
 
             if (typeof asString === "undefined"){
                 return "";
