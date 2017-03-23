f2f8969c92a07e0b0bd66fec88697b9a6f66e90c
Brooke <brooke@noredink.com>
2017-03-23 13:41:54 -0700

    fixes the children query via lots of pasted code

--- src/Test/Html/Selector/Internal.elm
+++ src/Test/Html/Selector/Internal.elm
@@ -1,7 +1,8 @@
 module Test.Html.Selector.Internal exposing (..)
 
-import ElmHtml.InternalTypes exposing (ElmHtml)
-import ElmHtml.Query
+import Dict
+import ElmHtml.InternalTypes exposing (..)
+import ElmHtml.Query exposing (Selector(..))
 
 
 type Selector
@@ -52,23 +53,152 @@ queryAll selectors list =
                 |> queryAll rest
 
 
+getChildren : ElmHtml -> List ElmHtml
+getChildren node =
+    case node of
+        NodeEntry { children } ->
+            children
+
+        _ ->
+            []
+
+
 queryOnlyChildren : List Selector -> List ElmHtml -> List ElmHtml
 queryOnlyChildren selectors list =
-    let
-        querySelectors =
-            List.map toQuerySelector selectors
+    applySelectors selectors (List.concatMap getChildren list)
 
-        parentNode =
-            List.head list
 
-        dropParentIfIncluded elements =
-            if List.head elements == parentNode then
-                List.drop 1 elements
-            else
-                elements
-    in
-        List.concatMap (ElmHtml.Query.queryChildrenAll querySelectors) list
-            |> dropParentIfIncluded
+applySelectors : List Selector -> List ElmHtml -> List ElmHtml
+applySelectors selectors elements =
+    case selectors of
+        [] ->
+            elements
+
+        selector :: rest ->
+            List.filter (trySelector selector) elements
+                |> applySelectors rest
+
+
+trySelector : Selector -> ElmHtml -> Bool
+trySelector selector node =
+    predicateFromSelector (toQuerySelector selector) node
+
+
+predicateFromSelector : ElmHtml.Query.Selector -> ElmHtml -> Bool
+predicateFromSelector selector html =
+    case html of
+        NodeEntry record ->
+            record
+                |> nodeRecordPredicate selector
+
+        _ ->
+            False
+
+
+hasAttribute : String -> String -> Facts -> Bool
+hasAttribute attribute query facts =
+    case Dict.get attribute facts.stringAttributes of
+        Just id ->
+            id == query
+
+        Nothing ->
+            False
+
+
+hasBoolAttribute : String -> Bool -> Facts -> Bool
+hasBoolAttribute attribute value facts =
+    case Dict.get attribute facts.boolAttributes of
+        Just id ->
+            id == value
+
+        Nothing ->
+            False
+
+
+classnames : Facts -> List String
+classnames facts =
+    Dict.get "className" facts.stringAttributes
+        |> Maybe.withDefault ""
+        |> String.split " "
+
+
+hasClass : String -> Facts -> Bool
+hasClass query facts =
+    List.member query (classnames facts)
+
+
+hasClasses : List String -> Facts -> Bool
+hasClasses classList facts =
+    containsAll classList (classnames facts)
+
+
+containsAll : List a -> List a -> Bool
+containsAll a b =
+    b
+        |> List.foldl (\i acc -> List.filter ((/=) i) acc) a
+        |> List.isEmpty
+
+
+hasAllSelectors : List ElmHtml.Query.Selector -> ElmHtml -> Bool
+hasAllSelectors selectors record =
+    List.map predicateFromSelector selectors
+        |> List.map (\selector -> selector record)
+        |> List.all identity
+
+
+nodeRecordPredicate : ElmHtml.Query.Selector -> (NodeRecord -> Bool)
+nodeRecordPredicate selector =
+    case selector of
+        Id id ->
+            .facts
+                >> hasAttribute "id" id
+
+        ClassName classname ->
+            .facts
+                >> hasClass classname
+
+        ClassList classList ->
+            .facts
+                >> hasClasses classList
+
+        ElmHtml.Query.Tag tag ->
+            .tag
+                >> (==) tag
+
+        ElmHtml.Query.Attribute key value ->
+            .facts
+                >> hasAttribute key value
+
+        ElmHtml.Query.BoolAttribute key value ->
+            .facts
+                >> hasBoolAttribute key value
+
+        ContainsText text ->
+            always False
+
+        Multiple selectors ->
+            NodeEntry
+                >> hasAllSelectors selectors
+
+
+
+-- queryOnlyChildren : List Selector -> List ElmHtml -> List ElmHtml
+-- queryOnlyChildren selectors list =
+--     let
+--         querySelectors =
+--             List.map toQuerySelector selectors
+--
+--         parentNode =
+--             List.head list
+--
+--         dropParentIfIncluded elements =
+--             if List.head elements == parentNode then
+--                 List.drop 1 elements
+--             else
+--                 elements
+--     in
+--         List.concatMap (ElmHtml.Query.queryChildrenAll querySelectors) list
+--             |> dropParentIfIncluded
 
 
 toQuerySelector : Selector -> ElmHtml.Query.Selector
