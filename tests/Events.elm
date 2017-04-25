67f5639bbb653b0f9c9142c7d2e03c5ac313739a
Rogerio Chaves <rogeriocfj@gmail.com>
2017-04-25 07:55:01 -0300

    Add Html.Map example, it works but gives the unmapped Msg

--- tests/Events.elm
+++ tests/Events.elm
@@ -1,53 +1,49 @@
 module Events exposing (..)
 
-import Html exposing (Html, div, button, text)
-import Html.Lazy as Lazy
-import Html.Attributes as Attr exposing (href)
-import Html.Events exposing (onClick)
-import Test.Html.Query as Query exposing (Single)
-import Test.Html.Events as Events
-import Test.Html.Selector exposing (tag)
-import Test exposing (..)
 import Expect
+import Html exposing (Html, button, div, text)
+import Html.Attributes as Attr exposing (href)
+import Html.Events exposing (onClick, onInput)
+import Html.Lazy as Lazy
+import Test exposing (..)
+import Test.Html.Events as Events
+import Test.Html.Query as Query exposing (Single)
+import Test.Html.Selector exposing (tag)
 
 
 all : Test
 all =
-    let
-        html =
-            [ htmlOutput, lazyOutput ]
-    in
-        Test.concat <|
-            List.concatMap (\f -> List.map f html)
-                [ testEvents
-                ]
-
-
-testEvents : Single -> Test
-testEvents output =
     describe "trigerring events"
         [ test "triggers click on element" <|
             \() ->
-                output
+                Query.fromHtml sampleHtml
                     |> Query.findAll [ tag "button" ]
                     |> Query.first
                     |> Events.trigger "click" "{}"
                     |> Expect.equal (Ok SampleMsg)
+        , test "triggers click on lazy html" <|
+            \() ->
+                Query.fromHtml sampleLazyHtml
+                    |> Query.findAll [ tag "button" ]
+                    |> Query.first
+                    |> Events.trigger "click" "{}"
+                    |> Expect.equal (Ok SampleMsg)
+        , test "triggers click on mapped html" <|
+            \() ->
+                Query.fromHtml sampleMappedHtml
+                    |> Query.findAll [ tag "button" ]
+                    |> Query.first
+                    |> Events.trigger "click" "{}"
+                    -- TODO: Html.Map is ignored when traversing the DOM, need to fix it to avoid repeated mapping on tests like this
+                    |>
+                        Result.map (always MappedSampleMsg)
+                    |> Expect.equal (Ok MappedSampleMsg)
         ]
-
-
-htmlOutput : Single
-htmlOutput =
-    Query.fromHtml sampleHtml
-
-
-lazyOutput : Single
-lazyOutput =
-    Query.fromHtml sampleLazyHtml
 
 
 type Msg
     = SampleMsg
+    | MappedSampleMsg
 
 
 sampleHtml : Html Msg
@@ -63,4 +59,11 @@ sampleLazyHtml =
         [ Lazy.lazy
             (\str -> button [ onClick SampleMsg ] [ text str ])
             "click me"
+        ]
+
+
+sampleMappedHtml : Html Msg
+sampleMappedHtml =
+    div [ Attr.class "container" ]
+        [ Html.map (always MappedSampleMsg) (button [ onClick SampleMsg ] [ text "click me" ])
         ]
