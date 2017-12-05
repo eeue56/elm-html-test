3b6fcd579f9c7991520263f0a0713329e2a3c270
Juan Edi <juan@noredink.com>
2017-12-05 11:57:20 -0300

    Force thunks under mapped html

--- src/Native/HtmlAsJson.js
+++ src/Native/HtmlAsJson.js
@@ -6,6 +6,9 @@ var _eeue56$elm_html_test$Native_HtmlAsJson = (function() {
         if (typeof vNode !== 'undefined' && vNode.type === 'thunk' && !vNode.node) {
             vNode.node = vNode.thunk.apply(vNode.thunk, vNode.args);
         }
+        if (typeof vNode !== 'undefined' && vNode.type === 'tagger') {
+            vNode.node = forceThunks(vNode.node);
+        }
         if (typeof vNode !== 'undefined' && typeof vNode.children !== 'undefined') {
             vNode.children = vNode.children.map(forceThunks);
         }
